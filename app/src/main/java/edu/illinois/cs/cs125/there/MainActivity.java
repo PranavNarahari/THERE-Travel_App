460bdb4fe10f789b8b22ffcb38e063c972237c24
PranavNarahari <pranavn2@illinois.edu>
2018-06-23 13:29:16 -0400

    Initial commit

---
+++ app/src/main/java/edu/illinois/cs/cs125/there/MainActivity.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs.cs125.there;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+}
