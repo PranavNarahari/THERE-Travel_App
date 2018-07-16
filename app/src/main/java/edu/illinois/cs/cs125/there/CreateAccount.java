87e4f8c942e4e6cc7bc50317617d133c3829cffa
SwatTeam <swathya100@gmail.com>
2018-07-16 13:50:02 -0400

    Added Push capiability to the buttons

---
+++ app/src/main/java/edu/illinois/cs/cs125/there/CreateAccount.java
@@ -0,0 +1,14 @@
+package edu.illinois.cs.cs125.there;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class CreateAccount extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_create_account);
+    }
+}
--- app/src/main/java/edu/illinois/cs/cs125/there/LoginActivity.java
+++ app/src/main/java/edu/illinois/cs/cs125/there/LoginActivity.java
@@ -1,13 +1,38 @@
 package edu.illinois.cs.cs125.there;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class LoginActivity extends AppCompatActivity {
+    private Button button3;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        button3 = findViewById(R.id.button3);
+        button3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v){
+                openCreateAccount();
+
+            }
+        });
+    }
+
+    public LoginActivity(Button button3) {
+        this.button3 = button3;
+    }
+
+    public void openCreateAccount() {
+        Intent intent = new Intent(this, CreateAccount.class);
+        startActivity(intent);
+
     }
 }
+
+
